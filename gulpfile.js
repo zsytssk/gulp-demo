6239f91cb950cc95a387c8581f0c84e40dd37967
zsytssk <zsytssk@gmail.com>
2016-08-15 15:47:01 +0800

    add some file

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,31 @@
 var gulp = require('gulp');
 var runPostcss = require('./gulp/tasks/postcss');
 var runPug = require('./gulp/tasks/pug');
+var concatjs = require('./gulp/tasks/concatjs');
 
 global.dirs = {
   src: './src',
   dist: './dist',
 };
 
+gulp.task('concatjs', concatjs);
 gulp.task('css', runPostcss);
 gulp.task('pug', runPug);
 
+var keyArr = [];
+var activeTask = {
+  'concatjs': '/**/*.js',
+  'css': '/**/*.css',
+  'pug': '/**/*.pug'
+};
+
 gulp.task('watch', function () {
-  gulp.watch(dirs.src + '/**/*.css', gulp.parallel('css'));
-  gulp.watch(dirs.src + '/**/*.pug', gulp.parallel('pug'));
+  for (var key in activeTask) {
+    gulp.watch(dirs.src + activeTask[key], gulp.parallel(key));
+  }
 });
 
+for (var key in activeTask) {
+  keyArr.push(key);
+}
 gulp.task('default', gulp.parallel('pug', 'css', 'watch'));
